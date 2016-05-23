a7ccc59e71e84d11bba0f63b9b55a6a53e4011b4
Fedor B Gorsky <fedor@support-pc.org>
2016-05-23 11:57:54 +0300

    Admin crud controller

---
+++ models/NotificationSearch.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace fgh151\notifications\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Notification;
+
+/**
+ * NotificationSearch represents the model behind the search form about `common\models\Notification`.
+ */
+class NotificationSearch extends Notification
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['Id', 'UserId'], 'integer'],
+            [['Message', 'DateSend'], 'safe'],
+            [['Read'], 'boolean'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Notification::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'Id' => $this->Id,
+            'UserId' => $this->UserId,
+            'DateSend' => $this->DateSend,
+            'Read' => $this->Read,
+        ]);
+
+        $query->andFilterWhere(['like', 'Message', $this->Message]);
+
+        return $dataProvider;
+    }
+}
